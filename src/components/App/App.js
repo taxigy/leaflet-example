9db58114ee562a2a63ece603e6d176d764be3b4a
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-08-08 14:28:59 +0400

    made Leaflet show up on the page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import {
+  Map,
+  TileLayer
+} from 'react-leaflet';
 import styles from './App.scss';
 
 export default class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        I AM THE APP K
+        <Map
+          className={styles.map}
+          center={[0, 0]}
+          zoom={5}
+          scrollWheelZoom={false}>
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='k tnx' />
+        </Map>
       </div>
     );
   }
