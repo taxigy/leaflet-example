5ecbc906a026a345479d9f7a36df1d67cf275395
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-08-12 16:39:41 +0400

    listening to wheel events and panning map

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
-import {
-  Map,
-  TileLayer
-} from 'react-leaflet';
+import L from 'leaflet';
 import styles from './App.scss';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const {
+      mapNode
+    } = this;
+    const map = L.map('map', {
+      center: [47.59401, -122.24406],
+      maxZoom: 17,
+      zoom: 16,
+      zoomControl: false,
+      attributionControl: false,
+      scrollWheelZoom: false
+    });
+    const tiles = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}');
+
+    tiles.addTo(map);
+
+    mapNode.addEventListener('wheel', (e) => {
+      e.preventDefault();
+      
+      const c = map.getCenter();
+      map.panTo({
+        lat: c.lat + e.wheelDeltaY / 100000,
+        lng: c.lng + e.wheelDeltaX / -100000
+      }, {
+        animate: true
+      });
+    });
+  }
+
   render() {
     return (
       <div className={styles.app}>
-        <Map
+        <div
+          className={styles.map}
+          id="map"
+          ref={node => {
+            this.mapNode = node;
+          }} />
+        {`<Map
           className={styles.map}
           center={[0, 0]}
           zoom={5}
+          zoomControl={false}
+          attributionControl={false}
           scrollWheelZoom={false}>
           <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
             attribution='k tnx' />
-        </Map>
+        </Map>`}
       </div>
     );
   }
